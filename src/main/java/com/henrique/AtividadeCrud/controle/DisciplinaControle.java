f0a93e19f1aa94b228536f616c88a1060c67a6ee
Henrique Moreira <henriquemoreiraa@gmail.com>
2020-12-12 21:42:56 -0300

    criação da parte create

--- src/main/java/com/henrique/AtividadeCrud/controle/DisciplinaControle.java
+++ src/main/java/com/henrique/AtividadeCrud/controle/DisciplinaControle.java
@@ -3,6 +3,7 @@ package com.henrique.AtividadeCrud.controle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.henrique.AtividadeCrud.dao.DisciplinaDAO;
 import com.henrique.AtividadeCrud.dominio.Disciplina;
@@ -18,5 +19,23 @@ public class DisciplinaControle {
 		List<Disciplina> lista = dao.todos();
 		modelo.addAttribute("lista",lista);
 		return "disciplinas"; 
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/cadastra-disciplina")
+	public String exibeForm(Model modelo) {
+		modelo.addAttribute("disciplina", new Disciplina());
+		return "form-disciplina";
+	}
+	
+	@PostMapping("/cadastra-disciplina")
+	public String processaForm(Disciplina disciplina) {
+		DisciplinaDAO dao = new DisciplinaDAO();
+		dao.inserir(disciplina);
+		return "redirect:/disciplinas";
 	}
 }
